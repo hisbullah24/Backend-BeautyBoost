88931e83e6768a4e048ef7b382ff77ec6209e454
Said Ali <saidalinh37@gmail.com>
2024-06-08 09:51:33 +0700

    second commit

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import db from "./config/Database.js";
 import fileUpload from "express-fileupload";
-import cors from "cors";
 import userRoute from "./routes/userRoute.js";
 import predictRoute from "../ExpressJS-API/routes/predictRoute.js";
 
+dotenv.config();
 const app = express();
 
 try {
     await db.authenticate();
     console.log('Database Connected ...');
 } catch (error) {
-    console.log(error);
+    console.error(error);
 }
 
-app.use(userRoute);
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({ credentials:true }));
 app.use(express.json());
 app.use(fileUpload());
 app.use(express.static("public"));
+app.use(userRoute);
 app.use(predictRoute);
 
-app.listen(5000, ()=> console.log('Server Up and Running...'));
+app.listen(5000, ()=> console.log('Server Up and Running on port 5000'));
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { Register, getUsers } from "../controllers/Users.js";
+import { getUsers, Login, Logout, Register } from "../controllers/Users.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+import { refreshToken } from "../controllers/refreshToken.js";
 
 const router = express.Router();
 
-router.get('/users', getUsers);
-router.post('/users', Register);
+router.get('/users', verifyToken, getUsers);
+router.post('/register', Register);
+router.post('/login', Login);
+router.get('/token', refreshToken);
+router.delete('/logout', Logout);
 
 export default router;
